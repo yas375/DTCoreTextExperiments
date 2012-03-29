0b2d4ecf8e37430c4fe516e67a84ff72d2a02c09
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2012-03-29 18:58:55 +0300

    initial commit. just create new project in xcode

---
+++ DTCoreTextExperiments/ADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADAppDelegate.h
+//  DTCoreTextExperiments
+//
+//  Created by Victor Ilyukevich on 29.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
